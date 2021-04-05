a19452bad677616ab10eb41397eeb9e79efa211f
developer-AAKASH <201912082@daiict.ac.in>
2021-04-06 00:08:28 +0530

    Initial commit with basic boiler-plate structure.

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const App = ()=>{
+	return (
+		<>
+			<Text>
+				Welcome to Street-Shopping !!!!
+			</Text>
+		</>
+	);
+};
+
+export default App;
